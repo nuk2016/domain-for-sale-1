749e207635337ce5baadcf91a7bb7318e59e664a
nuk2016 <nukleon99@hotmail.com>
2021-08-12 20:10:02 +0400

    Update mssg.js

--- mssg.js
+++ mssg.js
@@ -1,4 +1,4 @@
-var name = "acordonet";
-var domain = "gmail.com";
+var name = "then11websiteforsale";
+var domain = "outlook.com";
 document.write('<a href=\"mailto:' + name + '@' + domain + '\">');
 document.write(name + '@' + domain + '</a>');
